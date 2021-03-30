b58e16bf9d179bce749ebaa63080ed816e308adf
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-03-30 14:23:11 +1100

    Docker build working

--- docker_build/xx_code_scraps.sh
+++ docker_build/xx_code_scraps.sh
@@ -1,18 +1,12 @@
 
-
 # 1. build image
 setproxy
 cd /Users/$(whoami)/git/iag_geo/valhalla/docker_build
 docker build --tag iag-geo/valhalla:3.1.0 .
 
 # 2. run container
+docker run --tty --name="valhalla" --publish=8002:8002 iag-geo/valhalla:3.1.0
 
-
-# 3. log into container (default folder is /build)
-
-
-# 4. download OSM data and create tiles
-#mkdir -p valhalla_tiles
-/usr/local/bin/valhalla_build_tiles -c /build/valhalla.json /build/australia-latest.osm.pbf
-
-
+# 3. test a URL
+curl http://localhost:8002/route \
+--data '{"locations":[{"lat":-33.85,"lon":151.13,"type":"break","city":"Leichhardt","state":"NSW"},{"lat":-33.85,"lon":151.16,"type":"break","city":"Sydney","state":"NSW"}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
