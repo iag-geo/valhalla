252d03e4ea5f1eac1cd56dc3783eab605c0ec70d
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-03-31 12:20:10 +1100

    Kubernetes test deployment working

--- docker_build/xx_code_scraps.sh
+++ docker_build/xx_code_scraps.sh
@@ -4,22 +4,32 @@
 cd /Users/$(whoami)/git/iag_geo/valhalla/docker_build
 docker build --tag iag-geo/valhalla:3.1.0 .
 
-# 2. deploy image to a Kubernetes pod
+# 2. deploy to a Kubernetes pod
 kubectl create deployment valhalla --image=iag-geo/valhalla:3.1.0
-
-# scale deployment
-kubectl scale deployments/valhalla --replicas=4
-
-
 # create (i.e. expose) a service
 kubectl expose deployment/valhalla --type="NodePort" --port 8002
+# scale deployment
+kubectl scale deployments/valhalla --replicas=4
+# get the K8s node port number
+export NODE_PORT=$(kubectl get services/valhalla -o go-template='{{(index .spec.ports 0).nodePort}}')
+
+## 2. run container in Docker only
+#docker run --name=valhalla --publish=8002:8002 iag-geo/valhalla:3.1.0
+
+# 3. test a URL
+curl http://localhost:${NODE_PORT}/route \
+--data '{"locations":[{"lat":-33.85,"lon":151.13,"type":"break","city":"Leichhardt","state":"NSW"},{"lat":-33.85,"lon":151.16,"type":"break","city":"Sydney","state":"NSW"}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
 
 
 
-
+# Kubernetes code scraps
 
 # check status
+kubectl describe services/valhalla
+
 kubectl get deployments
+kubectl get pods -o wide
+
 kubectl describe deployment
 kubectl get pods -l run=valhalla
 kubectl get services -l run=valhalla
@@ -27,29 +37,15 @@ kubectl get services -l run=valhalla
 # change service label (optional)
 kubectl label pod $POD_NAME app=valhalla
 
-export NODE_PORT=$(kubectl get services/valhalla -o go-template='{{(index .spec.ports 0).nodePort}}')
-
 # delete service (app is still running inside pod!)
 kubectl delete service -l run=kubernetes-bootcamp
 
 # create proxy to access app in pod (NOT required if service is created i.e. exposed)
 kubectl proxy
 
-curl http://localhost:8001/version
-
 kubectl get pods -o go-template --template '{{range .items}}{{.mtadata.name}}{{"\n"}}{{end}}'
-
-
 
 kubectl logs
 
 # create Bash session inside pod
 kubectl exec -ti $POD_NAME bash
-
-
-## 2. run container
-#docker run --name=valhalla --publish=8002:8002 iag-geo/valhalla:3.1.0
-
-# 3. test a URL
-curl http://localhost:8002/route \
---data '{"locations":[{"lat":-33.85,"lon":151.13,"type":"break","city":"Leichhardt","state":"NSW"},{"lat":-33.85,"lon":151.16,"type":"break","city":"Sydney","state":"NSW"}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
