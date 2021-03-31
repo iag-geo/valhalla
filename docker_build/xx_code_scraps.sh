39ddbd19b2d3cfa850ddd04ca43c4698c921656d
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-03-31 12:39:03 +1100

    Kubernetes test deployment working

--- docker_build/xx_code_scraps.sh
+++ docker_build/xx_code_scraps.sh
@@ -37,6 +37,12 @@ kubectl get services -l run=valhalla
 # change service label (optional)
 kubectl label pod $POD_NAME app=valhalla
 
+# update image in pod on the fly (without down time) - will be "successful" even if image doesn't exist or work
+kubectl set image deployments/valhalla valhalla=iag-geo/valhalla:3.1.0
+
+# roll back image update changes if images don't exist or don't work
+kubectl rollout undo deployments/valhalla
+
 # delete service (app is still running inside pod!)
 kubectl delete service -l run=kubernetes-bootcamp
 
