0d1d471bf97cf949ec7484f829578d73109cc431
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-03-31 14:22:11 +1100

    Working on EC2 Docker/k8s install & deploy

---
+++ docker_build/00_build_and_deploy_locally.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# pre-requisite: install Docker Desktop first
+# https://www.docker.com/products/docker-desktop
+
+# 1. build image
+#setproxy
+cd /Users/$(whoami)/git/iag_geo/valhalla/docker_build
+docker build --tag iag-geo/valhalla:3.1.0 .
+
+# 2. deploy to a Kubernetes pod
+kubectl create deployment valhalla --image=iag-geo/valhalla:3.1.0
+# create (i.e. expose) a service
+kubectl expose deployment/valhalla --type="NodePort" --port 8002
+# scale deployment
+kubectl scale deployments/valhalla --replicas=4
+# get the k8s node port number
+export NODE_PORT=$(kubectl get services/valhalla -o go-template='{{(index .spec.ports 0).nodePort}}')
+
+## 2. run container in Docker only
+#docker run --name=valhalla --publish=8002:8002 iag-geo/valhalla:3.1.0
+
+# 3. test a URL
+curl http://localhost:${NODE_PORT}/route \
+--data '{"locations":[{"lat":-33.85,"lon":151.13,"type":"break","city":"Leichhardt","state":"NSW"},{"lat":-33.85,"lon":151.16,"type":"break","city":"Sydney","state":"NSW"}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
--- docker_build/xx_kubernetes_code_scraps.sh
+++ docker_build/xx_kubernetes_code_scraps.sh
@@ -1,30 +1,5 @@
 #!/usr/bin/env bash
 
-# 1. build image
-#setproxy
-cd /Users/$(whoami)/git/iag_geo/valhalla/docker_build
-docker build --tag iag-geo/valhalla:3.1.0 .
-
-# 2. deploy to a Kubernetes pod
-kubectl create deployment valhalla --image=iag-geo/valhalla:3.1.0
-# create (i.e. expose) a service
-kubectl expose deployment/valhalla --type="NodePort" --port 8002
-# scale deployment
-kubectl scale deployments/valhalla --replicas=4
-# get the K8s node port number
-export NODE_PORT=$(kubectl get services/valhalla -o go-template='{{(index .spec.ports 0).nodePort}}')
-
-## 2. run container in Docker only
-#docker run --name=valhalla --publish=8002:8002 iag-geo/valhalla:3.1.0
-
-# 3. test a URL
-curl http://localhost:${NODE_PORT}/route \
---data '{"locations":[{"lat":-33.85,"lon":151.13,"type":"break","city":"Leichhardt","state":"NSW"},{"lat":-33.85,"lon":151.16,"type":"break","city":"Sydney","state":"NSW"}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
-
-
-
-# Kubernetes code scraps
-
 # delete all Valhalla pods
 kubectl get pods --no-headers=true | awk '/valhalla/{print $1}' | xargs kubectl delete pod
 
@@ -55,7 +30,6 @@ kubectl delete service -l run=kubernetes-bootcamp
 
 # create proxy to access app in pod (NOT required if service is created i.e. exposed)
 kubectl proxy
-
 
 #kubectl logs
 
