192dac0ea05c82c6bc8fbedb053b708a9cec4aba
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-05-20 14:07:15 +1000

    changes for valhalla 3.1.1

--- docker_build/xx_code_snippets.sh
+++ docker_build/xx_code_snippets.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cd /Users/s57405/git/iag_geo/valhalla/docker_build
+cd /Users/hugh.saalmans/git/iag_geo/valhalla/docker_build
 
-docker build --tag minus34/valhalla:latest --tag minus34/valhalla:3.1.0 .
+docker build --tag minus34/valhalla:latest --tag minus34/valhalla:3.1.1 .
 
 docker run --name=valhalla --publish=8002:8002 minus34/valhalla:latest
