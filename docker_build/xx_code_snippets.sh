e81bb53c01c205ea6e723e2965d4ac4e153271a5
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-05-24 13:14:10 +1000

    build improvements

--- docker_build/xx_code_snippets.sh
+++ docker_build/xx_code_snippets.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+# 1. go to Dockerfile directory
 cd /Users/hugh.saalmans/git/iag_geo/valhalla/docker_build
 
+# 2. build the image
 # note: --squash is still an experimental docker feature (removes intermediate layers from final image)
-docker build --squash --tag minus34/valhalla:latest --tag minus34/valhalla:3.1.1 .
+docker build --squash --tag minus34/valhalla:latest . | tee ./valhalla-image-build.log
 
+# 3. run a container
 docker run --name=valhalla --publish=8002:8002 minus34/valhalla:latest
 
+# 4. clean up Docker locally - note: this could accidentally destroy your resources
+docker system prune
 
+# 5. test URL
 curl http://localhost:8002/route \
 --data '{"locations":[{"lat":-33.85,"lon":151.13,"type":"break","city":"Leichhardt","state":"NSW"},{"lat":-33.85,"lon":151.16,"type":"break","city":"Sydney","state":"NSW"}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
