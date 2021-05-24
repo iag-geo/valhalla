f350191e4aac39c00618bec861bd72b772048394
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-05-24 14:10:54 +1000

    added logging to image build

--- docker_build/xx_code_snippets.sh
+++ docker_build/xx_code_snippets.sh
@@ -5,7 +5,7 @@ cd /Users/hugh.saalmans/git/iag_geo/valhalla/docker_build
 
 # 2. build the image
 # note: --squash is still an experimental docker feature (removes intermediate layers from final image)
-docker build --squash --tag minus34/valhalla:latest . | tee ./valhalla-image-build.log
+docker build --squash --tag minus34/valhalla:latest .
 
 # 3. run a container
 docker run --name=valhalla --publish=8002:8002 minus34/valhalla:latest
