35d3e1eb96cf3e4b3644c075df2a320b3fc721bb
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-06-10 18:04:01 +1000

    improved port forwarding

--- docker_build/00_build_and_deploy_locally.sh
+++ docker_build/00_build_and_deploy_locally.sh
@@ -25,8 +25,10 @@ kubectl scale deployments/valhalla --replicas=4
 # wait for service to start
 sleep 30
 
-# port forward from Kubernetes to all local IPs (to enable external requests)
-kubectl port-forward service/valhalla 8002:8002 --address=0.0.0.0 &
+# port forward from Kubernetes to all IPs (to enable external requests over port 8002)
+# run in background to allow script to complete
+pf_cmd="kubectl port-forward service/valhalla 8002:8002 --address=0.0.0.0"
+eval "${pf_cmd}" &>/dev/null & disown;
 
 # wait for service to start
 sleep 30
