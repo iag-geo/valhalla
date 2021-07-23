a895e8c11048fa1b6bc2e40ee6ed2386ad65cd5a
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-07-23 12:27:55 +1000

    final route stats added

--- map_matching/postgres_scripts/06_stitch_routes.sql
+++ map_matching/postgres_scripts/06_stitch_routes.sql
@@ -129,26 +129,40 @@ WITH stats AS (
              gps_accuracy,
              st_y(geom)::numeric(7, 5),
              st_x(geom)::numeric(8, 5)
+), stats2 AS (
+    SELECT stats.trip_id,
+           stats.search_radius,
+           stats.gps_accuracy,
+           map_match_segments,
+           map_match_distance_km,
+           route_segments,
+           route_distance_km,
+           st_setsrid(st_makeline(pnt3.geom ORDER BY point_index), 4326) AS geom
+    FROM stats
+             INNER JOIN pnt3 ON stats.trip_id = pnt3.trip_id
+        AND stats.search_radius = pnt3.search_radius
+        AND stats.gps_accuracy = pnt3.gps_accuracy
+    GROUP BY stats.trip_id,
+             stats.search_radius,
+             stats.gps_accuracy,
+             map_match_segments,
+             map_match_distance_km,
+             route_segments,
+             route_distance_km
 )
-SELECT stats.trip_id,
-       stats.search_radius,
-       stats.gps_accuracy,
+SELECT trip_id,
+       search_radius,
+       gps_accuracy,
+       map_match_segments + route_segments AS total_segments,
+       (map_match_distance_km + route_distance_km)::numeric(8, 3) AS total_distance_km,
+       (map_match_distance_km / (map_match_distance_km + route_distance_km) * 100.0)::numeric(4, 1) AS map_match_percent,
        map_match_segments,
-       map_match_distance_km,
+       map_match_distance_km::numeric(8, 3) AS map_match_distance_km,
        route_segments,
-       route_distance_km,
-       st_setsrid(st_makeline(pnt3.geom ORDER BY point_index), 4326) AS geom
-FROM stats
-INNER JOIN pnt3 ON stats.trip_id = pnt3.trip_id
-    AND stats.search_radius = pnt3.search_radius
-    AND stats.gps_accuracy = pnt3.gps_accuracy
-GROUP BY stats.trip_id,
-         stats.search_radius,
-         stats.gps_accuracy,
-         map_match_segments,
-         map_match_distance_km,
-         route_segments,
-         route_distance_km
+       route_distance_km::numeric(8, 3) AS route_distance_km,
+       0.0::double precision AS rmse_km,
+       geom
+FROM stats2
 ;
 ANALYSE testing.valhalla_final_route;
 
@@ -158,46 +172,47 @@ CREATE INDEX valhalla_final_route_geom_idx ON testing.valhalla_final_route USING
 ALTER TABLE testing.valhalla_final_route CLUSTER ON valhalla_final_route_geom_idx;
 
 
+-- Calculate root mean square of distance from each waypoint to the final route
+--   As a proxy for reliability of both the input GPS points and the final route
+WITH merge AS (
+    SELECT trip.trip_id,
+           trip.search_radius,
+           trip.gps_accuracy,
+           st_distance( pnt.geom::geography, ST_ClosestPoint(trip.geom, pnt.geom)::geography) / 1000.0 AS route_point_distance_m
+    FROM testing.valhalla_final_route AS trip
+    INNER JOIN testing.waypoint AS pnt ON trip.trip_id = pnt.trip_id
+), stat AS (
+    SELECT trip_id,
+           search_radius,
+           gps_accuracy,
+           sqrt(sum(pow(route_point_distance_m, 2)))::numeric(8, 3) AS rmse_km
+    FROM merge
+    GROUP BY trip_id,
+             search_radius,
+             gps_accuracy
+)
+UPDATE testing.valhalla_final_route as route
+    SET rmse_km = stat.rmse_km
+FROM stat
+WHERE route.trip_id = stat.trip_id
+  AND route.search_radius = stat.search_radius
+  AND route.gps_accuracy = stat.gps_accuracy
+;
+
+
+
+-- DROP TABLE IF EXISTS testing.temp_split_shape;
+
+
 -- -- 1344 test rows
 -- SELECT count(*) FROM testing.temp_split_shape;
 -- SELECT count(*) FROM testing.valhalla_segments;
 
--- DROP TABLE IF EXISTS testing.temp_split_shape;
 
--- select *,
---        geometrytype(geom)
+
+-- select *
+-- --        geometrytype(geom)
 -- from testing.valhalla_final_route
 -- order by trip_id,
 --          search_radius,
 --          gps_accuracy;
-
-
-
-
--- -- add interpolated segments where a route segment used a different start point to the prevsious map matched end point
--- INSERT INTO testing.valhalla_segments
--- WITH seg AS (
---     SELECT trip_id,
---            search_radius,
---            gps_accuracy,
---            segment_index,
---            distance_m,
---            point_count,
---            segment_type,
---            st_endpoint(lag(geom) OVER (PARTITION BY trip_id, search_radius, gps_accuracy ORDER BY segment_index)) as previous_end_geom,
---            st_startpoint(geom) as start_geom,
---     FROM testing.valhalla_segments
--- )
--- SELECT trip_id,
---        search_radius,
---        gps_accuracy,
---        segment_index + 99999 AS segment_index,
---        st_distance(previous_end_geom::geography, start_geom::geography) AS distance_m,
---        2 AS point_count,
---        'interpolated' AS segment_type,
---        st_setsrid(st_makeLine(previous_end_geom,start_geom), 4326) AS geom
--- FROM seg
--- WHERE segment_type = 'route'
---   AND NOT st_equals(previous_end_geom, start_geom)
--- ;
--- ANALYSE testing.valhalla_segments;
