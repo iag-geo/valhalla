4cc6cd577479497812918883a7c78932b3b677e1
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-07-27 17:34:47 +1000

    refactoring route input table again...

--- map_matching/postgres_scripts/04_split_routes.sql
+++ map_matching/postgres_scripts/04_split_routes.sql
@@ -180,125 +180,127 @@ ANALYSE testing.temp_line_point;
 
 
 
--- STEP 2 - calculate bearing, reverse bearing and distance to create a splitting line perpendicular to the trip at each waypoint
---   Determine the azimuth based on +/- 90 degrees from trip direction at each waypoint
-DROP TABLE IF EXISTS temp_line_calc;
-CREATE TEMPORARY TABLE temp_line_calc AS
-WITH adjust AS (
-    SELECT *,
-           CASE WHEN trip_point_percent = 1.0 THEN trip_point_percent - 0.00011 ELSE
-               CASE WHEN trip_point_percent = 0.0 THEN trip_point_percent + 0.0001 ELSE trip_point_percent END
-           END AS trip_point_percent_fix
-    FROM testing.temp_line_point
-
-), az AS (
-    SELECT *,
-           ST_Azimuth(trip_point_geom,
-               ST_LineInterpolatePoint(trip_geom, trip_point_percent_fix + 0.0001)) + pi() / 2.0 AS line_azimuth
-    FROM adjust
---     WHERE trip_point_percent > 0.0
---         AND trip_point_percent < 0.9999 -- don't want start or end points of trip
-), fix AS ( -- fix azimuth if > 360 degrees (2 x Pi radians)
-    SELECT *,
-           CASE WHEN line_azimuth > pi() * 2.0 THEN line_azimuth - pi() * 2.0 ELSE line_azimuth END AS azimuthAB
-    FROM az
-)
-SELECT trip_id,
-       point_index,
-       begin_shape_index,
-       end_shape_index,
-       route_point_type,
-       search_radius,
-       gps_accuracy,
-       trip_point_percent,
-       trip_point_percent_fix,
-       geom,
-       azimuthAB,
-       CASE WHEN azimuthAB >= pi() THEN azimuthAB - pi() ELSE azimuthAB + pi() END AS azimuthBA,
-       ST_Distance(geom, trip_point_geom) + 0.00002 AS dist
-FROM fix
-;
-ANALYSE temp_line_calc;
-
-
--- STEP 3 - create the splitting lines that cross the map matched route
-DROP TABLE IF EXISTS testing.temp_split_line;
-CREATE TABLE testing.temp_split_line AS
-SELECT DISTINCT trip_id,
-                point_index,
-                begin_shape_index,
-                end_shape_index,
-                route_point_type,
-                search_radius,
-                gps_accuracy,
-                ST_MakeLine(ST_Translate(geom, sin(azimuthBA) * 0.00002, cos(azimuthBA) * 0.00002),
-                    ST_Translate(geom, sin(azimuthAB) * dist, cos(azimuthAB) * dist))::geometry(Linestring, 4326) AS geom
-FROM temp_line_calc
-;
-ANALYSE testing.temp_split_line;
-
+-- -- STEP 2 - calculate bearing, reverse bearing and distance to create a splitting line perpendicular to the trip at each waypoint
+-- --   Determine the azimuth based on +/- 90 degrees from trip direction at each waypoint
+-- DROP TABLE IF EXISTS temp_line_calc;
+-- CREATE TEMPORARY TABLE temp_line_calc AS
+-- WITH adjust AS (
+--     SELECT *,
+--            CASE WHEN trip_point_percent = 1.0 THEN trip_point_percent - 0.00011 ELSE
+--                CASE WHEN trip_point_percent = 0.0 THEN trip_point_percent + 0.0001 ELSE trip_point_percent END
+--            END AS trip_point_percent_fix
+--     FROM testing.temp_line_point
+--
+-- ), az AS (
+--     SELECT *,
+--            ST_Azimuth(trip_point_geom,
+--                ST_LineInterpolatePoint(trip_geom, trip_point_percent_fix + 0.0001)) + pi() / 2.0 AS line_azimuth
+--     FROM adjust
+-- --     WHERE trip_point_percent > 0.0
+-- --         AND trip_point_percent < 0.9999 -- don't want start or end points of trip
+-- ), fix AS ( -- fix azimuth if > 360 degrees (2 x Pi radians)
+--     SELECT *,
+--            CASE WHEN line_azimuth > pi() * 2.0 THEN line_azimuth - pi() * 2.0 ELSE line_azimuth END AS azimuthAB
+--     FROM az
+-- )
+-- SELECT trip_id,
+--        point_index,
+--        begin_shape_index,
+--        end_shape_index,
+--        route_point_type,
+--        search_radius,
+--        gps_accuracy,
+--        trip_point_percent,
+--        trip_point_percent_fix,
+--        geom,
+--        azimuthAB,
+--        CASE WHEN azimuthAB >= pi() THEN azimuthAB - pi() ELSE azimuthAB + pi() END AS azimuthBA,
+--        ST_Distance(geom, trip_point_geom) + 0.00002 AS dist
+-- FROM fix
+-- ;
+-- ANALYSE temp_line_calc;
+--
+--
+-- -- STEP 3 - create the splitting lines that cross the map matched route
+-- DROP TABLE IF EXISTS testing.temp_split_line;
+-- CREATE TABLE testing.temp_split_line AS
+-- SELECT DISTINCT trip_id,
+--                 point_index,
+--                 begin_shape_index,
+--                 end_shape_index,
+--                 route_point_type,
+--                 search_radius,
+--                 gps_accuracy,
+--                 ST_MakeLine(ST_Translate(geom, sin(azimuthBA) * 0.00002, cos(azimuthBA) * 0.00002),
+--                     ST_Translate(geom, sin(azimuthAB) * dist, cos(azimuthAB) * dist))::geometry(Linestring, 4326) AS geom
+-- FROM temp_line_calc
+-- ;
+-- ANALYSE testing.temp_split_line;
+--
 
 select * from testing.temp_line_point;
-select * from temp_line_calc;
-
-select * from testing.temp_split_line;
 
 
-
--- STEP 4 - split the matched routes into a new table
-DROP TABLE IF EXISTS testing.temp_split_shape;
-CREATE TABLE testing.temp_split_shape AS
-WITH blade AS (
-    SELECT trip_id,
-           search_radius,
-           gps_accuracy,
-           begin_shape_index,
-           st_collect(geom) AS geom
-    FROM testing.temp_split_line
-    GROUP BY trip_id,
-             search_radius,
-             gps_accuracy,
-             begin_shape_index
-), split AS (
-    SELECT trip.trip_id,
-           trip.begin_shape_index,
-           trip.end_shape_index,
-           trip.search_radius,
-           trip.gps_accuracy,
-           st_split(trip.geom, blade.geom) AS geom
-    FROM testing.valhalla_map_match_shape AS trip
-    INNER JOIN blade ON trip.trip_id = blade.trip_id
-        AND trip.search_radius = blade.search_radius
-        AND trip.gps_accuracy = blade.gps_accuracy
-        AND trip.begin_shape_index = blade.begin_shape_index
-), lines AS (
-    SELECT trip_id,
-           search_radius,
-           gps_accuracy,
-           begin_shape_index,
-           end_shape_index,
-           (ST_Dump(geom)).path[1] AS segment_index,
-           (ST_Dump(geom)).geom    AS geom
-    FROM split
-)
-SELECT trip_id,
-       search_radius,
-       gps_accuracy,
-       begin_shape_index,
-       end_shape_index,
-       segment_index,
-       st_length(geom::geography) AS distance_m,
-       st_npoints(geom)           AS point_count,
-       'map match'::text          AS segment_type,
-       geom
-FROM lines
-;
-ANALYSE testing.temp_split_shape;
-
-ALTER TABLE testing.temp_split_shape
-    ADD CONSTRAINT temp_split_shape_pkey PRIMARY KEY (trip_id, search_radius, gps_accuracy, begin_shape_index, segment_index);
-CREATE INDEX temp_split_shape_geom_idx ON testing.temp_split_shape USING gist (geom);
-ALTER TABLE testing.temp_split_shape CLUSTER ON temp_split_shape_geom_idx;
+-- select * from temp_line_calc;
+--
+-- select * from testing.temp_split_line;
+--
+--
+--
+-- -- STEP 4 - split the matched routes into a new table
+-- DROP TABLE IF EXISTS testing.temp_split_shape;
+-- CREATE TABLE testing.temp_split_shape AS
+-- WITH blade AS (
+--     SELECT trip_id,
+--            search_radius,
+--            gps_accuracy,
+--            begin_shape_index,
+--            st_collect(geom) AS geom
+--     FROM testing.temp_split_line
+--     GROUP BY trip_id,
+--              search_radius,
+--              gps_accuracy,
+--              begin_shape_index
+-- ), split AS (
+--     SELECT trip.trip_id,
+--            trip.begin_shape_index,
+--            trip.end_shape_index,
+--            trip.search_radius,
+--            trip.gps_accuracy,
+--            st_split(trip.geom, blade.geom) AS geom
+--     FROM testing.valhalla_map_match_shape AS trip
+--     INNER JOIN blade ON trip.trip_id = blade.trip_id
+--         AND trip.search_radius = blade.search_radius
+--         AND trip.gps_accuracy = blade.gps_accuracy
+--         AND trip.begin_shape_index = blade.begin_shape_index
+-- ), lines AS (
+--     SELECT trip_id,
+--            search_radius,
+--            gps_accuracy,
+--            begin_shape_index,
+--            end_shape_index,
+--            (ST_Dump(geom)).path[1] AS segment_index,
+--            (ST_Dump(geom)).geom    AS geom
+--     FROM split
+-- )
+-- SELECT trip_id,
+--        search_radius,
+--        gps_accuracy,
+--        begin_shape_index,
+--        end_shape_index,
+--        segment_index,
+--        st_length(geom::geography) AS distance_m,
+--        st_npoints(geom)           AS point_count,
+--        'map match'::text          AS segment_type,
+--        geom
+-- FROM lines
+-- ;
+-- ANALYSE testing.temp_split_shape;
+--
+-- ALTER TABLE testing.temp_split_shape
+--     ADD CONSTRAINT temp_split_shape_pkey PRIMARY KEY (trip_id, search_radius, gps_accuracy, begin_shape_index, segment_index);
+-- CREATE INDEX temp_split_shape_geom_idx ON testing.temp_split_shape USING gist (geom);
+-- ALTER TABLE testing.temp_split_shape CLUSTER ON temp_split_shape_geom_idx;
 
 
 -- STEP 5 - get start and end points of segments to be routed
@@ -306,32 +308,46 @@ ALTER TABLE testing.temp_split_shape CLUSTER ON temp_split_shape_geom_idx;
 DROP TABLE IF EXISTS testing.temp_route_this;
 CREATE TABLE testing.temp_route_this AS
 WITH starts AS (
-    SELECT row_id,
-           trip_id,
-           search_radius,
-           gps_accuracy,
-           point_index,
-           geom
-    FROM testing.temp_line_point
-    WHERE route_point_type = 'start'
+    SELECT pnt.trip_id,
+           pnt.search_radius,
+           pnt.gps_accuracy,
+           pnt.point_index,
+           pnt.begin_shape_index,
+           pnt.end_shape_index,
+           st_endpoint(trip.geom) AS geom
+    FROM testing.temp_line_point AS pnt
+             INNER JOIN testing.valhalla_map_match_shape AS trip
+                        ON pnt.trip_id = trip.trip_id
+                            AND pnt.search_radius = trip.search_radius
+                            AND pnt.gps_accuracy = trip.gps_accuracy
+                            AND pnt.begin_shape_index = trip.end_shape_index
+    WHERE pnt.route_point_type = 'start'
 ), ends AS (
-    SELECT row_id,
-           trip_id,
-           search_radius,
-           gps_accuracy,
-           point_index,
-           geom
-    FROM testing.temp_line_point
-    WHERE route_point_type = 'end'
+    SELECT pnt.trip_id,
+           pnt.search_radius,
+           pnt.gps_accuracy,
+           pnt.point_index,
+           pnt.begin_shape_index,
+           pnt.end_shape_index,
+           st_startpoint(trip.geom) AS geom
+    FROM testing.temp_line_point AS pnt
+             INNER JOIN testing.valhalla_map_match_shape AS trip
+                        ON pnt.trip_id = trip.trip_id
+                            AND pnt.search_radius = trip.search_radius
+                            AND pnt.gps_accuracy = trip.gps_accuracy
+                            AND pnt.end_shape_index = trip.begin_shape_index
+    WHERE pnt.route_point_type = 'end'
 )
 SELECT starts.trip_id,
        starts.search_radius,
        starts.gps_accuracy,
-       row_number()
-       OVER (PARTITION BY starts.trip_id, starts.search_radius, starts.gps_accuracy ORDER BY starts.point_index) *
-       2                  AS segment_index,
+--        row_number()
+--        OVER (PARTITION BY starts.trip_id, starts.search_radius, starts.gps_accuracy ORDER BY starts.point_index) *
+--        2                  AS segment_index,
        starts.point_index AS start_point_index,
        ends.point_index   AS end_point_index,
+       starts.begin_shape_index,
+       starts.end_shape_index,
 --        st_distance(starts.geom::geography, ends.geom::geography) AS distance_m,
        st_y(starts.geom)  AS start_lat,
        st_x(starts.geom)  AS start_lon,
@@ -343,26 +359,75 @@ FROM starts
          INNER JOIN ends ON starts.trip_id = ends.trip_id
     AND starts.search_radius = ends.search_radius
     AND starts.gps_accuracy = ends.gps_accuracy
-    AND starts.row_id = ends.row_id - 1 -- get sequential pairs of start & end records
+    AND starts.begin_shape_index = ends.begin_shape_index
 ;
 ANALYSE testing.temp_route_this;
 
--- need to adjust segment indexes where first route segment is at the start of the trip
-WITH fix AS (
-    SELECT trip_id,
-           search_radius,
-           gps_accuracy
-    FROM testing.temp_route_this
-    WHERE start_point_index = 0
-)
-UPDATE testing.temp_route_this AS route
-SET segment_index = segment_index - 2
-FROM fix
-WHERE route.trip_id = fix.trip_id
-  AND route.search_radius = fix.search_radius
-  AND route.gps_accuracy = fix.gps_accuracy
-;
-ANALYSE testing.temp_route_this;
+
+-- -- STEP 5 - get start and end points of segments to be routed
+-- --   Do this by flattening pairs of start & end points
+-- DROP TABLE IF EXISTS testing.temp_route_this;
+-- CREATE TABLE testing.temp_route_this AS
+-- WITH starts AS (
+--     SELECT row_id,
+--            trip_id,
+--            search_radius,
+--            gps_accuracy,
+--            point_index,
+--            geom
+--     FROM testing.temp_line_point
+--     WHERE route_point_type = 'start'
+-- ), ends AS (
+--     SELECT row_id,
+--            trip_id,
+--            search_radius,
+--            gps_accuracy,
+--            point_index,
+--            geom
+--     FROM testing.temp_line_point
+--     WHERE route_point_type = 'end'
+-- )
+-- SELECT starts.trip_id,
+--        starts.search_radius,
+--        starts.gps_accuracy,
+--        row_number()
+--        OVER (PARTITION BY starts.trip_id, starts.search_radius, starts.gps_accuracy ORDER BY starts.point_index) *
+--        2                  AS segment_index,
+--        starts.point_index AS start_point_index,
+--        ends.point_index   AS end_point_index,
+-- --        st_distance(starts.geom::geography, ends.geom::geography) AS distance_m,
+--        st_y(starts.geom)  AS start_lat,
+--        st_x(starts.geom)  AS start_lon,
+--        st_y(ends.geom)    AS end_lat,
+--        st_x(ends.geom)    AS end_lon,
+--        starts.geom        AS start_geom,
+--        ends.geom          AS end_geom
+-- FROM starts
+--          INNER JOIN ends ON starts.trip_id = ends.trip_id
+--     AND starts.search_radius = ends.search_radius
+--     AND starts.gps_accuracy = ends.gps_accuracy
+--     AND starts.row_id = ends.row_id - 1 -- get sequential pairs of start & end records
+-- ;
+-- ANALYSE testing.temp_route_this;
+
+
+-- -- need to adjust segment indexes where first route segment is at the start of the trip
+-- WITH fix AS (
+--     SELECT trip_id,
+--            search_radius,
+--            gps_accuracy
+--     FROM testing.temp_route_this
+--     WHERE start_point_index = 0
+-- )
+-- UPDATE testing.temp_route_this AS route
+-- SET segment_index = segment_index - 2
+-- FROM fix
+-- WHERE route.trip_id = fix.trip_id
+--   AND route.search_radius = fix.search_radius
+--   AND route.gps_accuracy = fix.gps_accuracy
+-- ;
+-- ANALYSE testing.temp_route_this;
+
 
 -- select trip_id,
 --        search_radius,
