6361776f85850beecfcf0cc8a1b04fff982702c8
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-08-04 17:57:54 +1000

    fixing route stitching bug

--- map_matching/postgres_scripts/06_stitch_routes.sql
+++ map_matching/postgres_scripts/06_stitch_routes.sql
@@ -8,28 +8,37 @@ ANALYSE testing.temp_valhalla_segments;
 
 -- create primary key to ensure uniqueness
 ALTER TABLE testing.temp_valhalla_segments
-    ADD CONSTRAINT temp_valhalla_segments_pkey PRIMARY KEY (trip_id, search_radius, gps_accuracy, begin_edge_index, end_edge_index);
+    ADD CONSTRAINT temp_valhalla_segments_pkey PRIMARY KEY (trip_id, search_radius, gps_accuracy, begin_edge_index);
 
+-- todo: fix this issue
 -- CREATE UNIQUE INDEX temp_valhalla_segments_end_shape_index_idx ON testing.temp_valhalla_segments USING btree (trip_id, search_radius, gps_accuracy, end_shape_index);
 
 -- Add map matched segments that haven't been fixed by routed
 INSERT INTO testing.temp_valhalla_segments
-SELECT temp.trip_id,
-       temp.search_radius,
-       temp.gps_accuracy,
-       temp.edge_index AS begin_edge_index,
-       temp.edge_index AS end_edge_index,
-       temp.begin_shape_index,
-       temp.end_shape_index,
-       temp.distance_m,
-       st_numpoints(temp.geom) as point_count,
+SELECT trip_id,
+       search_radius,
+       gps_accuracy,
+       edge_index AS begin_edge_index,
+       edge_index AS end_edge_index,
+       begin_shape_index,
+       end_shape_index,
+       distance_m,
+       st_numpoints(geom) as point_count,
        'map match' AS segment_type,
-       temp.geom
+       geom
 FROM testing.valhalla_map_match_shape AS temp
-INNER JOIN testing.temp_valhalla_segments AS seg ON seg.trip_id = temp.trip_id
-    AND seg.search_radius = temp.search_radius
-    AND seg.gps_accuracy = temp.gps_accuracy
-    AND temp.edge_index NOT BETWEEN seg.begin_edge_index AND seg.end_edge_index
+WHERE NOT EXISTS(
+        SELECT trip_id,
+               search_radius,
+               gps_accuracy,
+               begin_shape_index,
+               end_shape_index
+        FROM testing.temp_valhalla_segments AS seg
+        WHERE seg.trip_id = temp.trip_id
+          AND seg.search_radius = temp.search_radius
+          AND seg.gps_accuracy = temp.gps_accuracy
+          AND temp.edge_index = seg.begin_edge_index
+    )
 ;
 ANALYSE testing.temp_valhalla_segments;
 
