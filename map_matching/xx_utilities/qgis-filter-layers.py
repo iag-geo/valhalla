696d0467cdfbf1486967f09a45abb552b3d262c0
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-08-04 11:57:06 +1000

    added QGIS filter multiple layers script

--- map_matching/xx_utilities/qgis-filter-layers.py
+++ map_matching/xx_utilities/qgis-filter-layers.py
@@ -7,13 +7,15 @@ filter_text = "trip_id = '918E16D3-709F-44DE-8D9B-78F8C6981122' and and search_r
 extent = QgsRectangle()
 extent.setMinimal()
 layers = iface.mapCanvas().layers()
+
+# filter matching layers
 for layer in layers:
     # if layer.name() not in ignore_layers:
-    if layer.name().startswith(layer_prefixes[0]) or layer.name().startswith(layer_prefixes[1]:
+    if layer.name().startswith(layer_prefixes[0]) or layer.name().startswith(layer_prefixes[1]):
         layer.setSubsetString(filter_text)
         extent.combineExtentWith( layer.extent() )
 
-# update extents of filtered dataset for "Zoom to Layer" to work
+# update extents of filtered datasets for "Zoom to Layer" to work
 sourceCrs = QgsCoordinateReferenceSystem("EPSG:4283")
 destCrs = QgsCoordinateReferenceSystem("EPSG:900913")
 tr = QgsCoordinateTransform(sourceCrs, destCrs, QgsProject.instance())
