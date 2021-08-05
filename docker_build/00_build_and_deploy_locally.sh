2b78d27f0e53b015147569aa8bbfd377b0b9719f
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-08-05 18:53:39 +1000

    improved image build

--- docker_build/00_build_and_deploy_locally.sh
+++ docker_build/00_build_and_deploy_locally.sh
@@ -10,21 +10,21 @@ cd $HOME
 # 1. go to Dockerfile directory
 cd /Users/$(whoami)/git/iag_geo/valhalla/docker_build
 
-# 2. build the image
-# note: --squash is still an experimental docker feature (removes intermediate layers from final image)
-docker build --squash --tag minus34/valhalla:latest .
-
-## 3. run a container
-#docker run --name=valhalla --publish=8002:8002 minus34/valhalla:latest
-
-# 4. push to Docker Hub
-docker push minus34/valhalla:latest
+## 2. build the image
+## note: --squash is still an experimental docker feature (removes intermediate layers from final image)
+#docker build --squash --tag minus34/valhalla:latest .
+#
+### 3. run a container
+##docker run --name=valhalla --publish=8002:8002 minus34/valhalla:latest
+#
+## 4. push to Docker Hub
+#docker push minus34/valhalla:latest
 
 ## 4. clean up Docker locally - note: this could accidentally destroy your resources
 #docker system prune
 
 # OR DOWNLOAD
-#docker pull minus34/valhalla:latest
+docker pull minus34/valhalla:latest
 
 # ------------------------------------------------------------------------------------------------------------
 # 2. deploy using Kubernetes
@@ -56,6 +56,3 @@ curl http://localhost:8002/route \
 #curl http://localhost:8002/route \
 #--data '{"locations":[{"lat":-33.85,"lon":151.13},{"lat":-33.85,"lon":151.16}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
 
-
-## 2. run container in Docker only
-#docker run --name=valhalla --publish=8002:8002 minus34/valhalla:latest
