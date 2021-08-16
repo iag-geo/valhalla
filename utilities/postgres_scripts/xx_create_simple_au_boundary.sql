e55ca0f2edb59d30b70eaee7694ecac7a16bcbe8
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-08-17 09:36:00 +1000

    testing optimal AU bdy parameters

--- utilities/postgres_scripts/xx_create_simple_au_boundary.sql
+++ utilities/postgres_scripts/xx_create_simple_au_boundary.sql
@@ -1,29 +1,59 @@
 
 
+-- -- buffer states out & then in to create a ~500-1500m buffer to exclude invalid AU GPS data with
+-- -- 11 mins
+-- drop table if exists testing.simple_au_bdy_2;
+-- create table testing.simple_au_bdy_2 as
+-- with bdy as (
+--     select st_setsrid(st_buffer(geom::geography, 1000.0, 1)::geometry, 4326) as geom
+--     from admin_bdys_202102.state_bdys
+-- -- where st_area(geom::geography) > 1000000.0  -- 1 sq km
+-- )
+-- select st_setsrid(st_buffer(st_union(geom)::geography, -500.0, 1)::geometry, 4326) as geom
+-- from bdy
+-- ;
+
 -- buffer states out & then in to create a ~500-1500m buffer to exclude invalid AU GPS data with
 -- 11 mins
-drop table if exists testing.simple_au_bdy;
-create table testing.simple_au_bdy as
+drop table if exists testing.simple_au_bdy_2;
+create table testing.simple_au_bdy_2 as
 with bdy as (
-    select st_setsrid(st_buffer(geom::geography, 5000.0)::geometry, 4326) as geom
+    select st_setsrid(st_buffer(geom::geography, 300.0, 1)::geometry, 4326) as geom
     from admin_bdys_202102.state_bdys
 -- where st_area(geom::geography) > 1000000.0  -- 1 sq km
 )
-select st_setsrid(st_buffer(st_union(geom)::geography, -5000.0, 1)::geometry, 4326) as geom
+select st_union(geom) as geom
 from bdy
 ;
 
--- split into single polygons and subdivide the complex ones for faster processing
-drop table if exists testing.simple_au_bdy_analysis;
-create table testing.simple_au_bdy_analysis as
+
+-- -- split into single polygons and subdivide the complex ones for faster processing
+-- drop table if exists testing.simple_au_bdy_2_analysis;
+-- create table testing.simple_au_bdy_2_analysis as
+-- with bdy as (
+--     select (st_dump(geom)).geom as geom
+--     from testing.simple_au_bdy_2
+-- )
+-- select st_subdivide(geom, 256) as geom
+-- from bdy
+-- ;
+
+
+-- split into single polygons, simplify them and subdivide the complex ones for faster processing
+drop table if exists testing.simple_au_bdy_2_analysis;
+create table testing.simple_au_bdy_2_analysis as
 with bdy as (
     select (st_dump(geom)).geom as geom
-    from testing.simple_au_bdy
+    from testing.simple_au_bdy_2
+), thin as (
+    select ST_SimplifyPreserveTopology(geom, 0.001) as geom
+    from bdy
 )
 select st_subdivide(geom, 256) as geom
-from bdy
+from thin
 ;
+
 
 
 -- 881 sections
-select count(*) from testing.simple_au_bdy_analysis;
+select count(*) from testing.simple_au_bdy_2_analysis;
