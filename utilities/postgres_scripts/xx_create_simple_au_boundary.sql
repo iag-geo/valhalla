fcc8a4d96d67c9fd83ab8d50e786e32e6d7282b4
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-08-17 18:25:42 +1000

    testing optimal AU bdy parameters

--- utilities/postgres_scripts/xx_create_simple_au_boundary.sql
+++ utilities/postgres_scripts/xx_create_simple_au_boundary.sql
@@ -2,37 +2,38 @@
 
 -- -- buffer states out & then in to create a ~500-1500m buffer to exclude invalid AU GPS data with
 -- -- 11 mins
--- drop table if exists testing.simple_au_bdy_2;
--- create table testing.simple_au_bdy_2 as
+-- drop table if exists testing.simple_au_bdy;
+-- create table testing.simple_au_bdy as
 -- with bdy as (
---     select st_setsrid(st_buffer(geom::geography, 1000.0, 1)::geometry, 4326) as geom
+--     select st_setsrid(st_buffer(geom::geography, 5000.0, 4)::geometry(POLYGON), 4326) as geom
 --     from admin_bdys_202102.state_bdys
 -- -- where st_area(geom::geography) > 1000000.0  -- 1 sq km
 -- )
--- select st_setsrid(st_buffer(st_union(geom)::geography, -500.0, 1)::geometry, 4326) as geom
+-- select st_setsrid(st_buffer(st_union(geom)::geography, -4500.0, 2)::geometry(MULTIPOLYGON), 4326) as geom
 -- from bdy
+-- where st_geometrytype(geom) IN ('POLYGON')
 -- ;
 
--- buffer states out & then in to create a ~500-1500m buffer to exclude invalid AU GPS data with
+-- buffer the coastline out & then in to create a ~500-1500m buffer to exclude invalid AU GPS data with
 -- 11 mins
 drop table if exists testing.simple_au_bdy_2;
 create table testing.simple_au_bdy_2 as
 with bdy as (
     select st_setsrid(st_buffer(geom::geography, 300.0, 1)::geometry, 4326) as geom
-    from admin_bdys_202102.state_bdys
+    from admin_bdys_202102.abs_2016_gccsa
 -- where st_area(geom::geography) > 1000000.0  -- 1 sq km
 )
 select st_union(geom) as geom
 from bdy
 ;
-
+ALTER TABLE testing.simple_au_bdy_2 ALTER COLUMN geom type geometry(MultiPolygon, 4326) USING ST_Multi(geom);
 
 -- -- split into single polygons and subdivide the complex ones for faster processing
--- drop table if exists testing.simple_au_bdy_2_analysis;
--- create table testing.simple_au_bdy_2_analysis as
+-- drop table if exists testing.simple_au_bdy_analysis;
+-- create table testing.simple_au_bdy_analysis as
 -- with bdy as (
 --     select (st_dump(geom)).geom as geom
---     from testing.simple_au_bdy_2
+--     from testing.simple_au_bdy
 -- )
 -- select st_subdivide(geom, 256) as geom
 -- from bdy
@@ -46,14 +47,17 @@ with bdy as (
     select (st_dump(geom)).geom as geom
     from testing.simple_au_bdy_2
 ), thin as (
-    select ST_SimplifyPreserveTopology(geom, 0.001) as geom
+    select ST_Simplify(geom, 0.002) as geom
     from bdy
 )
 select st_subdivide(geom, 256) as geom
-from thin
+from bdy
 ;
 
 
 
 -- 881 sections
-select count(*) from testing.simple_au_bdy_2_analysis;
+select count(*), st_geometrytype(geom) as geom_type
+from testing.simple_au_bdy_2_analysis
+group by st_geometrytype(geom)
+;
