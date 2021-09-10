f1a3a69d97245ff4b7bb9c02e45337d472d67c6b
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-09-10 11:45:54 +1000

    removed old files

--- map_matching/postgres_scripts/not_used/05_create_route_indexes.sql
+++
@@ -1,9 +0,0 @@
-
--- add indexes to Valhalla route output tables
-ALTER TABLE testing.valhalla_route_shape
-    ADD CONSTRAINT valhalla_route_shape_pkey PRIMARY KEY (trip_id, search_radius, gps_accuracy, begin_edge_index);
-CREATE INDEX valhalla_route_shape_geom_idx ON testing.valhalla_route_shape USING gist (geom);
-ALTER TABLE testing.valhalla_route_shape CLUSTER ON valhalla_route_shape_geom_idx;
-
-ALTER TABLE testing.valhalla_route_fail
-    ADD CONSTRAINT valhalla_route_fail_pkey PRIMARY KEY (trip_id, search_radius, gps_accuracy, begin_edge_index);
