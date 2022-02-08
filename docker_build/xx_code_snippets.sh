55e18993006307076768792c0bf87fe8368c46f6
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-02-09 10:22:27 +1100

    rejigged docker commands

--- docker_build/xx_code_snippets.sh
+++ docker_build/xx_code_snippets.sh
@@ -7,16 +7,17 @@ cd /Users/hugh.saalmans/git/iag_geo/valhalla/docker_build
 # note: --squash is still an experimental docker feature (removes intermediate layers from final image)
 docker build --squash --tag minus34/valhalla:latest --no-cache .
 
-# 3. run a container
-docker run --name=valhalla --publish=8002:8002 minus34/valhalla:latest
-
-# 4. push to Docker Hub
+# 3. push to Docker Hub
 docker push minus34/valhalla:latest
 
-# 5. clean up Docker locally - note: this could accidentally destroy your resources
-docker system prune
+# 4. clean up Docker locally - note: this could accidentally destroy your resources
+echo 'y' | docker system prune
 
 
-# 6. test URL
+
+# run a container
+docker run --name=valhalla --publish=8002:8002 minus34/valhalla:latest
+
+# test URL
 curl http://localhost:8002/route \
 --data '{"locations":[{"lat":-33.85,"lon":151.13,"type":"break","city":"Leichhardt","state":"NSW"},{"lat":-33.85,"lon":151.16,"type":"break","city":"Sydney","state":"NSW"}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
