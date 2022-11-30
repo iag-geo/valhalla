ff3a15a7277e52aa2aeba0c5982de85617cf7c10
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-11-30 12:25:12 +1100

    reverted port forward changes

--- docker_build/xx_kubernetes_code_scraps.sh
+++ docker_build/xx_kubernetes_code_scraps.sh
@@ -55,8 +55,8 @@ kubectl exec -ti $POD_NAME bash
 curl http://${INSTANCE_IP_ADDRESS}:8002/route \
 --data '{"locations":[{"lat":-33.85,"lon":151.13,"type":"break","city":"Leichhardt","state":"NSW"},{"lat":-33.85,"lon":151.16,"type":"break","city":"Sydney","state":"NSW"}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
 
-#curl http://localhost:8002/route \
-#--data '{"locations":[{"lat":-33.85,"lon":151.13,"type":"break","city":"Leichhardt","state":"NSW"},{"lat":-33.85,"lon":151.16,"type":"break","city":"Sydney","state":"NSW"}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
+curl http://localhost:8002/route \
+--data '{"locations":[{"lat":-33.85,"lon":151.13,"type":"break","city":"Leichhardt","state":"NSW"},{"lat":-33.85,"lon":151.16,"type":"break","city":"Sydney","state":"NSW"}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
 
 curl http://10.107.166.0:8002/route \
 --data '{"locations":[{"lat":-33.85,"lon":151.13,"type":"break","city":"Leichhardt","state":"NSW"},{"lat":-33.85,"lon":151.16,"type":"break","city":"Sydney","state":"NSW"}],"costing":"auto","directions_options":{"units":"kilometres"}}' | jq '.'
