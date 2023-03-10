1c39d8ea38943287faebb2ba0d20462edcdd278e
Hugh Saalmans <hugh.saalmans@gmail.com>
2023-03-10 13:26:19 +1100

    tweaks

--- docker_build/xx_build_docker_image.sh
+++ docker_build/xx_build_docker_image.sh
@@ -14,12 +14,12 @@ echo 'y' | docker system prune
 
 
 # run a container in the background
-docker run --detach --publish=8002:8002 minus34/valhalla:latest
+docker run --platform linux/amd64 --detach --publish=8002:8002 minus34/valhalla:latest
 
 
 # test URL
 curl http://localhost:8002/route \
---data '{"locations":[{"lat":-33.85,"lon":151.13},{"lat":-33.85,"lon":151.16}],"costing":"auto","directions_options":{"units":"kilometres"}}'
+--data '{"locations":[{"lat":-33.8799,"lon":151.1437, "radius":5},{"lat":-33.8679,"lon":151.12123, "radius":5}],"costing":"auto","directions_options":{"units":"kilometres"}}'
 
 ##test URL with JSON formatting (install using "brew install jq")
 #curl http://localhost:8002/route \
