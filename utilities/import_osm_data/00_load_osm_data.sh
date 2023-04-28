4434e3d1c6fd0b4965988f7971d2c485351bf953
Hugh Saalmans <hugh.saalmans@iag.com.au>
2023-04-28 16:07:13 +1000

    added schema prep

--- utilities/import_osm_data/00_load_osm_data.sh
+++ utilities/import_osm_data/00_load_osm_data.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+psql -d geo -p 5432 -U postgres -c "CREATE SCHEMA IF NOT EXISTS osm AUTHORIZATION postgres;"
+psql -d geo -p 5432 -U postgres -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+psql -d geo -p 5432 -U postgres -c "CREATE EXTENSION IF NOT EXISTS hstore;"
+
 cd /Users/$(whoami)/tmp
 
 curl https://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf -o australia-latest.osm.pbf
