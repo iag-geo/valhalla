a7f3efd6b404d19fe54862d76d969f86c555405a
Hugh Saalmans <hugh.saalmans@gmail.com>
2024-11-21 17:38:48 +1100

    added docker startup

--- docker_build/xx_build_docker_image.sh
+++ docker_build/xx_build_docker_image.sh
@@ -13,9 +13,6 @@ docker buildx inspect --bootstrap
 # 3. build and push images
 docker buildx build --platform linux/amd64,linux/arm64 --tag minus34/valhalla:latest . --push
 
-## 4. clean up Docker locally - note: this could accidentally destroy your resources
-#echo 'y' | docker system prune
-
 # run a container in the background
 docker run --detach --publish=8002:8002 minus34/valhalla:latest
 
@@ -23,6 +20,9 @@ docker run --detach --publish=8002:8002 minus34/valhalla:latest
 sleep 30
 curl http://localhost:8002/route --data '{"locations":[{"lat":-33.8799,"lon":151.1437, "radius":5},{"lat":-33.8679,"lon":151.12123, "radius":5}],"costing":"auto","directions_options":{"units":"kilometres"}}'
 
+
+# 4. clean up Docker locally - note: this could accidentally destroy your resources
+echo 'y' | docker system prune
 
 ##test URL with JSON formatting (install using "brew install jq")
 #curl http://localhost:8002/route \
