2b27c1432e5ecb0361c86f98312cba0a5ae1fdb3
Hugh Saalmans <hugh.saalmans@iag.com.au>
2025-09-25 08:08:23 +1000

    working on inferred maxspeeds

--- utilities/import_osm_data/01_filter_roads.sql
+++ utilities/import_osm_data/01_filter_roads.sql
@@ -106,7 +106,7 @@ with good as (
            geom
     from osm.osm_road
     where inferred_maxspeed is not null
-      and type not in ('service', 'unclassified')
+--       and type not in ('service', 'unclassified')
 ), bad as (
     select osm_id,
            inferred_maxspeed,
@@ -114,7 +114,7 @@ with good as (
            geom
     from osm.osm_road
     where inferred_maxspeed is null
-      and type not in ('service', 'unclassified')
+--       and type not in ('service', 'unclassified')
 --     limit 1000
 ), merge as (
     select bad.osm_id,
@@ -147,7 +147,7 @@ where osm.osm_id = merge2.osm_id
 ;
 
 
--- 2nd pass - to get roads that are connected to ones that got picked up in the 1st pass
+-- 2nd pass - to get roads that are connected to ones that got picked up in the 1st pass (fixes roundabouts amongst others)
 with good as (
     select osm_id,
            inferred_maxspeed,
@@ -155,7 +155,7 @@ with good as (
            geom
     from osm.osm_road
     where inferred_maxspeed is not null
-      and type not in ('service', 'unclassified')
+--       and type not in ('service', 'unclassified')
 ), bad as (
     select osm_id,
            inferred_maxspeed,
@@ -163,7 +163,7 @@ with good as (
            geom
     from osm.osm_road
     where inferred_maxspeed is null
-      and type not in ('service', 'unclassified')
+--       and type not in ('service', 'unclassified')
 --     limit 1000
 ), merge as (
     select bad.osm_id,
@@ -196,6 +196,53 @@ where osm.osm_id = merge2.osm_id
 ;
 
 
+-- TODO: add speed limits to _links (not including motorway_links)
+-- 3rd pass - to get roads that are connected to ones that got picked up in the 1st pass (fixes roundabouts amongst others)
+with good as (
+    select osm_id,
+           inferred_maxspeed,
+           replace(type, '_link', '') as type,
+           geom
+    from osm.osm_road
+    where inferred_maxspeed is not null
+), bad as (
+    select osm_id,
+           inferred_maxspeed,
+           replace(type, '_link', '') as type,
+           geom
+    from osm.osm_road
+    where inferred_maxspeed is null
+      and type <> 'motorway_link'
+      and type like '%_link'
+), merge as (
+    select bad.osm_id,
+           bad.type,
+           good.inferred_maxspeed,
+           count(distinct good.osm_id) as matches
+    from bad
+             inner join good on bad.type = good.type
+        and st_touches(bad.geom, good.geom)
+    group by bad.osm_id,
+             bad.type,
+             good.inferred_maxspeed
+), crunch as (
+    select osm_id,
+           type
+    from merge
+    group by osm_id,
+             type
+    having count(*) = 1
+), merge2 as (
+    select distinct merge.*
+    from crunch
+             inner join merge on crunch.osm_id = merge.osm_id
+)
+update osm.osm_road as osm
+set inferred_maxspeed = merge2.inferred_maxspeed,
+    inference_type = 'spatial pass 3'
+from merge2
+where osm.osm_id = merge2.osm_id
+;
 
 -- add 50 km/h to residential streets with no speed limit -- 357,085 rows
 update osm.osm_road
@@ -206,38 +253,38 @@ where inferred_maxspeed is null
 ;
 
 
--- 872708
-select inference_type,
-       type,
-       count(*)
-from osm.osm_road
-group by inference_type, type
-order by inference_type, type
-;
-
--- 98812
-select count(*)
-from osm.osm_road
-where inferred_maxspeed is null
-  and type not in ('service', 'unclassified', 'busway', 'living_street')
-;
-
-select type,
-       count(*)
-from osm.osm_road
-where inferred_maxspeed is null
-group by type
-order by type
-;
-
-select type,
-       junction,
-       count(*)
-from osm.osm_road
-where inferred_maxspeed is null
-group by type, junction
-order by type, junction
-;
+-- -- 872708
+-- select inference_type,
+--        type,
+--        count(*)
+-- from osm.osm_road
+-- group by inference_type, type
+-- order by inference_type, type
+-- ;
+--
+-- -- 98812
+-- select count(*)
+-- from osm.osm_road
+-- where inferred_maxspeed is null
+--   and type not in ('service', 'unclassified', 'busway', 'living_street')
+-- ;
+--
+-- select type,
+--        count(*)
+-- from osm.osm_road
+-- where inferred_maxspeed is null
+-- group by type
+-- order by type
+-- ;
+--
+-- select type,
+--        junction,
+--        count(*)
+-- from osm.osm_road
+-- where inferred_maxspeed is null
+-- group by type, junction
+-- order by type, junction
+-- ;
 
 
 
