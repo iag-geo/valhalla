c23e6614b990dc256fb5044df1f80432c1d0dd7b
Hugh Saalmans <hugh.saalmans@gmail.com>
2025-09-27 10:19:01 +1000

    updated .pbf file URL

--- utilities/import_osm_data/00_load_osm_data.sh
+++ utilities/import_osm_data/00_load_osm_data.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export PATH=/bin:/usr/bin:$PATH # Add /bin and /usr/bin to PATH
+
 # get the directory this script is running from
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -9,11 +11,16 @@ psql -d geo -p 5432 -U postgres -c "CREATE EXTENSION IF NOT EXISTS hstore;"
 
 cd /Users/$(whoami)/tmp
 
-curl https://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf -o australia-latest.osm.pbf
+## "latest" build no longer available - pick the build from 2 days ago to be safe (yesterday's may not exist yet)
+FORMATTED_DATE="$(($(date +'%y%m%d') - 2))"
+FILE_NAME="australia-${FORMATTED_DATE}.osm.pbf"
+#echo ${FILE_NAME}
+
+curl https://download.geofabrik.de/australia-oceania/${FILE_NAME} -o ${FILE_NAME}
 
 brew upgrade osm2pgsql
-osm2pgsql -c --cache 8096 --latlong --hstore --output-pgsql-schema osm --number-processes 12 -H 127.0.0.1 -P 5432 -U postgres -d geo australia-latest.osm.pbf
+osm2pgsql -c --latlong --hstore --output-pgsql-schema osm --number-processes 12 -H 127.0.0.1 -P 5432 -U postgres -d geo ${FILE_NAME}
 
-rm australia-latest.osm.pbf
+rm ${FILE_NAME}
 
 psql -d geo -p 5432 -U postgres -f ${SCRIPT_DIR}/01_filter_roads.sql
